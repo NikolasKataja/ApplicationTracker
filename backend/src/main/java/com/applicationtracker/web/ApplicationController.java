38b42c2718c48b41a5fcbff47f73a2c076f5093e
Nikolas Kataja <SHKPOSTISI>
2026-08-10 15:01:33 +0300

    Add POST and GET by id in ApplicationController

--- backend/src/main/java/com/applicationtracker/web/ApplicationController.java
+++ backend/src/main/java/com/applicationtracker/web/ApplicationController.java
@@ -3,8 +3,12 @@ package com.applicationtracker.web;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 import com.applicationtracker.domain.Application;
 import com.applicationtracker.repository.ApplicationRepository;
@@ -22,5 +26,17 @@ public class ApplicationController {
     @GetMapping
     public List<Application> getApplications() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Application> getApplicationById(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Application addApplication(@RequestBody Application application) {
+        return repository.save(application);
     }
 }
