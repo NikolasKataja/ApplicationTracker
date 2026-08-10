1cd5d159357d455a63531d455775de60eb43ff18
Nikolas Kataja <SHKPOSTISI>
2026-08-10 15:16:45 +0300

    Add PUT

--- backend/src/main/java/com/applicationtracker/web/ApplicationController.java
+++ backend/src/main/java/com/applicationtracker/web/ApplicationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,22 @@ public class ApplicationController {
     @PostMapping
     public Application addApplication(@RequestBody Application application) {
         return repository.save(application);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Application> updateApplication(
+        @PathVariable Long id,
+        @RequestBody Application updatedApplication) {
+
+            return repository.findById(id)
+                .map(application -> {
+                    application.setCompany(updatedApplication.getCompany());
+                    application.setPosition(updatedApplication.getPosition());
+                    application.setLocation(updatedApplication.getLocation());
+                    Application savedApplication = repository.save(application);
+                    return ResponseEntity.ok(savedApplication);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
